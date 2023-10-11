1dc83bdd3aeb1f8e81447ca4d0d436bc670cdb97
talihaarif <talihaarif13@gmail.com>
2023-10-11 16:51:47 +0500

    first commit

--- search-app-be/server.js
+++ search-app-be/server.js
@@ -7,7 +7,7 @@ const express = require('express');
 var cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const url = 'mongodb+srv://TalihaArif:bscs0102@cluster0.divek.mongodb.net/searchdb'; //use your own url to connect
+const url = ''; //use your own url to connect
 const app = express();
 mongoose.connect(url);
 const con = mongoose.connection;
